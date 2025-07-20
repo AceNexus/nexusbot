2619f428212bb14e12887465ae1c52768dcc617b
Ace <ooxxqaws@gmail.com>
2025-07-20 21:06:41 +0800

    [feat] 新增 webhook API

--- src/main/java/com/acenexus/tata/nexusbot/controller/LineBotController.java
+++ src/main/java/com/acenexus/tata/nexusbot/controller/LineBotController.java
@@ -1,13 +1,28 @@
 package com.acenexus.tata.nexusbot.controller;
 
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LineBotController {
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @GetMapping("test")
     public String test() {
         return "Test";
     }
+
+    @PostMapping("webhook")
+    public String receiveWebhook(@RequestBody String payload) throws Exception {
+        Object json = objectMapper.readValue(payload, Object.class);
+        String pretty = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        System.out.println("Webhook:\n" + pretty);
+        return "OK";
+    }
+
 }
